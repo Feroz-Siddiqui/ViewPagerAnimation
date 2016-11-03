a13dc12e7e00194ff9ddb91b568d34ab7400c069
Ajith <ajith@istarindia.com>
2016-11-03 19:06:00 +0530

    viewpager

--- app/src/main/java/com/example/feroz/androidcms/EndActivity.java
+++ app/src/main/java/com/example/feroz/androidcms/EndActivity.java
@@ -2,32 +2,73 @@ package com.example.feroz.androidcms;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.ImageView;
 
-import com.squareup.picasso.Picasso;
+import com.example.feroz.androidcms.cmsslide.CMSSlide;
+import com.example.feroz.androidcms.cmsslide.Presentation;
+import com.example.feroz.androidcms.viewpagerutility.LockableViewPager;
+
+import org.simpleframework.xml.Serializer;
+import org.simpleframework.xml.core.Persister;
+
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class EndActivity extends AppCompatActivity {
     private Button submit;
-    private ImageView imageView;
-    private Picasso mPicasso;
+    private  LockableViewPager viewPager;
+    private ArrayList<CMSSlide> cmsSlides;
+    private ViewPagerAdapter viewPagerAdapter;
+    static private int currentPage =0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end);
         submit = (Button) findViewById(R.id.submit);
-        imageView = (ImageView) findViewById(R.id.image);
+        viewPager = (LockableViewPager) findViewById(R.id.viewPager1);
+        try {
+            Serializer serializer = new Persister();
 
-        /*mPicasso = Picasso.with(this); //Single instance
+            Presentation s =serializer.read(Presentation.class,  getAssets().open("root.xml"));
+            cmsSlides = s.getCmslide();
 
-        mPicasso.load(R.drawable.theend)
-                .resize(200, 200).centerInside()
-                .error(context.getResources().getDrawable(R.mipmap.ic_business_black_48dp))
-                .placeholder(context.getResources().getDrawable(R.mipmap.ic_business_black_48dp)).into(imageView);
-*/
+        }catch(Exception e){
+            e.printStackTrace();
+
+        }
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(),cmsSlides);
+        viewPager.setAdapter(viewPagerAdapter);
+        viewPager.setCurrentItem(0);
+        viewPager.setSwipeLocked(true);
+        final Handler handler = new Handler();
+
+        final Runnable update = new Runnable() {
+            public void run() {
+                if (viewPager.getCurrentItem() == (viewPager.getAdapter().getCount()-1)) {
+                    currentPage = 0;
+                }
+                viewPager.setCurrentItem(currentPage++, true);
+            }
+        };
+
+
+        new Timer().schedule(new TimerTask() {
+
+            @Override
+            public void run() {
+                handler.post(update);
+            }
+        }, 100, 900);
 
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
